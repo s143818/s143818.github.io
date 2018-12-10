2eae28581ce14145ebdc302f4f7a2761e5394c2d
Michelle Lind Østrup <>
2018-12-10 13:34:49 +0100

    more stuff

--- js/main.js
+++ js/main.js
@@ -69,12 +69,42 @@ function main() {
         });
 
     });
+    
+    // Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items1');
+        $container.isotope({
+            filter: '.residential',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat1 a').click(function() {
+            $('.cat1 .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
 	
     // Nivo Lightbox 
     $('.portfolio-item a').nivoLightbox({
             effect: 'slideDown',  
             keyboardNav: true,                            
         });
+    
+    
 
 }());
 
