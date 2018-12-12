32cc95a2819c256f43a2a0f65de9184c8f9f4260
s143818 <s143818@student.dtu.dk>
2018-12-12 22:47:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,34 +45,50 @@ function main() {
     // Slider
     var slider = document.getElementById("myRange");
     var output = document.getElementById("demo");
+    var clique = document.getElementById("demo1");
+    var percentage = document.getElementById("percentage");
     output.innerHTML = slider.value;
-
+clique.innerHTML = slider.value;
+    percentage.innerHTML = "165";
     slider.oninput = function() {
       output.innerHTML = this.value;
+        clique.innerHTML = this.value;
       if (this.value == 3){
           $("#sliderimg").attr("src","img/clique03.png");
+          percentage.innerHTML = "838";
       } else if (this.value == 4){
           $("#sliderimg").attr("src","img/clique04.png");
+          percentage.innerHTML = "646";
       } else if (this.value == 5){
           $("#sliderimg").attr("src","img/clique05.png");
+          percentage.innerHTML = "490";
       } else if (this.value == 6){
           $("#sliderimg").attr("src","img/clique06.png");
+          percentage.innerHTML = "371";
       } else if (this.value == 7){
           $("#sliderimg").attr("src","img/clique07.png");
+          percentage.innerHTML = "289";
       } else if (this.value == 8){
           $("#sliderimg").attr("src","img/clique08.png");
+          percentage.innerHTML = "212";
       } else if (this.value == 9){
           $("#sliderimg").attr("src","img/clique09.png");
+          percentage.innerHTML = "165";
       } else if (this.value == 10){
           $("#sliderimg").attr("src","img/clique10.png");
+          percentage.innerHTML = "111";
       } else if (this.value == 11){
           $("#sliderimg").attr("src","img/clique11.png");
+          percentage.innerHTML = "73";
       } else if (this.value == 12){
           $("#sliderimg").attr("src","img/clique12.png");
+          percentage.innerHTML = "62";
       } else if (this.value == 13){
           $("#sliderimg").attr("src","img/clique13.png");
+          percentage.innerHTML = "40";
       } else if (this.value == 14){
           $("#sliderimg").attr("src","img/clique14Zoomed.png");
+          percentage.innerHTML = "21";
       }
         
     }
